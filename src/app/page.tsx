1deec3e7537b1b402866c1c73b10e4de344e73ed
Arafath  Imorou <arafathimorou@gmail.com>
2026-06-11 03:09:20 +0100

    fix: correct language to Bariba for TINDANOU SOULE BIO KARIMOU

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
     { id: 'local-8', title: "Maimounatou AROUNA", description: "Jeune femme Leader / Commune de DJOUGOU", duration: "", category: "Témoignages", language: "Dendi", video_url: "/capsule9.mp4" },
     { id: 'local-9', title: "KOTO Zibo", description: "Relais communautaire / Commune de Karimama", duration: "", category: "Témoignages", language: "DENDI", video_url: "/capsule10.mp4" },
     { id: 'local-10', title: "Djamaloul-Dine ALIDOU", description: "Membre du Rotary Club de Natitingou Tanguiéta", duration: "", category: "Rotary", language: "Français", video_url: "/capsule11.mp4" },
-    { id: 'local-11', title: "TINDANOU SOULE BIO KARIMOU", description: "Agent de Santé Communautaire Qualifié Goumori (Banikoara)", duration: "", category: "Témoignages", language: "Non précisé", video_url: "/capsule12.mp4" },
+    { id: 'local-11', title: "TINDANOU SOULE BIO KARIMOU", description: "Agent de Santé Communautaire Qualifié Goumori (Banikoara)", duration: "", category: "Témoignages", language: "Bariba", video_url: "/capsule12.mp4" },
     { id: 'local-12', title: "Abiba BIO YO", description: "Agent de Santé Communautaire Qualifié (ASCQ) Localité : OUNET (BANIKOARA)", duration: "", category: "Témoignages", language: "Bariba", video_url: "/capsule13.mp4" },
     { id: 'local-13', title: "Albéric GOUNOU", description: "Infirmier Diplômé d’État | Agent de Santé Communautaire Qualifié (ASCQ)", duration: "", category: "Témoignages", language: "Français", video_url: "/capsulealberic.mp4" }
   ];
